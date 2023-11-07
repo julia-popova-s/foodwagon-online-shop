7e153885b291ea6f06eebec886fc05f8391b08d7
Popova Julia <yuliapopova.s@yandex.ru>
2023-11-07 17:31:57 +0300

    added validation of registration/authorization form via react hook form

--- src/components/pages/Login/AuthRegForm.js
+++ src/components/pages/Login/AuthRegForm.js
@@ -1,24 +1,68 @@
-import { useState } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { string } from 'yup'
+
+import { regExpEmail } from '../../../utils/regExpEmail.js'
+import style from './authRegForm.module.scss'
+
+const schema = yup
+  .object({
+    email: string()
+      .matches(regExpEmail, { excludeEmptyString: true, message: 'email is in incorrect format' })
+      .required(),
+    password: yup.string().required().min(8),
+  })
+  .required()
 
 export function AuthRegForm({ handleClick, title }) {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const {
+    control,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  })
+  console.log(isValid)
+  const onSubmit = ({ email, password }) => handleClick(email, password)
 
   return (
-    <div>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="email"
-        type="email"
-        value={email}
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="password"
-        type="password"
-        value={password}
-      />
-      <button onClick={() => handleClick(email, password)}>{title}</button>
-    </div>
+    <form className={style.regForm} onSubmit={handleSubmit(onSubmit)}>
+      <label className={style.regForm__label}>
+        Email address
+        <Controller
+          render={({ field }) => (
+            <input {...field} aria-invalid={errors.email ? 'true' : 'false'} />
+          )}
+          className={style.regForm__input}
+          control={control}
+          name="email"
+        />
+      </label>
+      <p className={style.regForm__error} role="alert">
+        {errors.email?.message}{' '}
+      </p>
+
+      <label className={style.regForm__label}>
+        Password
+        <Controller
+          render={({ field }) => (
+            <input {...field} aria-invalid={errors.password ? 'true' : 'false'} />
+          )}
+          className={style.regForm__input}
+          control={control}
+          name="password"
+        />
+      </label>
+      <p className={style.regForm__error} role="alert">
+        {errors.password?.message}
+      </p>
+      <input className={style.regForm__button} type="submit" value={title} />
+    </form>
   )
 }
--- src/utils/validationEmail.js
+++ src/utils/validationEmail.js
@@ -1,5 +1,5 @@
+import { regExpEmail } from './regExpEmail.js.js'
+
 export const validationEmail = function (email) {
-  const reg =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return reg.test(String(email).toLowerCase())
+  return regExpEmail.test(String(email).toLowerCase())
 }
