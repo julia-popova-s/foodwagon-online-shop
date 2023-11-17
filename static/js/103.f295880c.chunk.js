841715f27e69a16b48950722c601cadf16f3327f
Popova Julia <yuliapopova.s@yandex.ru>
2023-11-17 14:46:08 +0300

    Updates

---
+++ static/js/103.f295880c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfoodwagon_project=self.webpackChunkfoodwagon_project||[]).push([[103],{2103:function(s,e,n){n.r(e),n.d(e,{default:function(){return t}});var a=n(2791),c=n(7689),r=n(4224),o=n(1823),u=n(184);var t=function(){return(0,u.jsxs)("div",{className:"app",children:[(0,u.jsx)(r.h4,{}),(0,u.jsx)(a.Suspense,{fallback:(0,u.jsx)(o.Z,{}),children:(0,u.jsx)(c.j3,{})}),(0,u.jsx)(r.$_,{})]})}}}]);
+//# sourceMappingURL=103.f295880c.chunk.js.map
