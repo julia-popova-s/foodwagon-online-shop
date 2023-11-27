e336cff147cfc4d83784532aba6f5bced424394e
Popova Julia <yuliapopova.s@yandex.ru>
2023-11-27 20:55:26 +0300

    updated mobile menu

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,25 +1,21 @@
 import cn from 'classnames';
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { ReactSVG } from 'react-svg';
 
-import { isAuthSelector } from '../../../store/reducers/user';
 import { removeUser } from '../../../store/reducers/user';
+import { isAuthSelector } from '../../../store/reducers/user';
 import { LogoType } from '../../ui/LogoType';
 import { ButtonCart } from '../../ui/buttons/ButtonCart';
 import { ButtonLogin } from '../../ui/buttons/ButtonLogin';
 import { CurrentLocation } from './CurrentLocation';
+import { MobileMenu } from './MobileMenu';
 import style from './header.module.scss';
 
 export function Header() {
-  const [menuIsVisible, setMenuIsVisible] = useState(false);
-
-  const menuRef = useRef(null);
-  const buttonRef = useRef(null);
-
   const navigate = useNavigate();
   const goBack = () => navigate('/');
 
@@ -36,21 +32,6 @@ export function Header() {
   const handleLogin = () => {
     navigate('/login');
   };
-
-  const handleClickMenu = () => {
-    setMenuIsVisible(!menuIsVisible);
-  };
-
-  useEffect(() => {
-    const handleOutsideClick = (e) => {
-      if (!menuRef.current?.contains(e.target) && !buttonRef.current?.contains(e.target)) {
-        setMenuIsVisible(false);
-      }
-    };
-    document.body.addEventListener('click', handleOutsideClick);
-
-    return () => document.body.removeEventListener('click', handleOutsideClick);
-  }, []);
 
   return (
     <header className={style.headerBlock}>
@@ -85,60 +66,7 @@ export function Header() {
             )}
           </div>
 
-          <button
-            className={cn(style.menuButton, {
-              [style.buttonClose]: menuIsVisible,
-            })}
-            onClick={handleClickMenu}
-            ref={buttonRef}
-          >
-            <div className={cn(style.menuButton__border, style.menuButton__border_top)}></div>
-            <div className={cn(style.menuButton__border, style.menuButton__border_center)}></div>
-            <div className={cn(style.menuButton__border, style.menuButton__border_bottom)}></div>
-          </button>
-
-          <nav
-            className={cn(style.menu, {
-              [style.menuClosed]: !menuIsVisible,
-              [style.menuOpened]: menuIsVisible,
-            })}
-            ref={menuRef}
-          >
-            <ul className={style.menu__list}>
-              <li className={style.menu__item}>
-                <Link className={style.menu__link} to={'/'}>
-                  Home
-                </Link>
-              </li>
-
-              <li className={style.menu__item}>
-                <Link className={style.menu__link} to={'/search'}>
-                  Search
-                </Link>
-              </li>
-
-              <li className={style.menu__item}>
-                {isAuth ? (
-                  <button className={style.menu__link} onClick={handleLogOut}>
-                    Logout
-                  </button>
-                ) : (
-                  <Link className={style.menu__link} to={'/login'}>
-                    Login
-                  </Link>
-                )}
-              </li>
-
-              <li className={style.menu__item}>
-                <Link className={style.menu__link} to={'/cart'}>
-                  Cart
-                </Link>
-              </li>
-              <li className={style.menu__item}>
-                <CurrentLocation />
-              </li>
-            </ul>
-          </nav>
+          <MobileMenu handleLogOut={handleLogOut} />
         </div>
       </div>
     </header>
---
+++ src/components/elements/Header/MobileMenu.js
@@ -0,0 +1,84 @@
+import cn from 'classnames';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { isAuthSelector } from '../../../store/reducers/user';
+import { CurrentLocation } from './CurrentLocation';
+import style from './mobileMenu.module.scss';
+
+export function MobileMenu({ handleLogOut, show }) {
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
+
+  const handleClickMenu = () => {
+    setMenuIsVisible(!menuIsVisible);
+  };
+
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  const isAuth = useSelector(isAuthSelector);
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (!menuRef.current?.contains(e.target) && !buttonRef.current?.contains(e.target)) {
+        setMenuIsVisible(false);
+      }
+    };
+    document.body.addEventListener('click', handleOutsideClick);
+
+    return () => document.body.removeEventListener('click', handleOutsideClick);
+  }, []);
+
+  return (
+    <div>
+      <button className={style.menuButton} onClick={handleClickMenu} ref={buttonRef}>
+        <div className={cn(style.menuButton__border, style.menuButton__border_top)}></div>
+        <div className={cn(style.menuButton__border, style.menuButton__border_center)}></div>
+        <div className={cn(style.menuButton__border, style.menuButton__border_bottom)}></div>
+      </button>
+
+      <nav
+        className={cn(style.menu, {
+          [style.menuClosed]: !menuIsVisible,
+        })}
+        ref={menuRef}
+      >
+        <ul className={style.menu__list}>
+          <li className={style.menu__item}>
+            <Link className={style.menu__link} to={'/'}>
+              Home
+            </Link>
+          </li>
+
+          <li className={style.menu__item}>
+            <Link className={style.menu__link} to={'/search'}>
+              Search
+            </Link>
+          </li>
+
+          <li className={style.menu__item}>
+            {isAuth ? (
+              <button className={style.menu__link} onClick={handleLogOut}>
+                Logout
+              </button>
+            ) : (
+              <Link className={style.menu__link} to={'/login'}>
+                Login
+              </Link>
+            )}
+          </li>
+
+          <li className={style.menu__item}>
+            <Link className={style.menu__link} to={'/cart'}>
+              Cart
+            </Link>
+          </li>
+          <li className={style.menu__item}>
+            <CurrentLocation />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
