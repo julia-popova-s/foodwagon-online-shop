03b7ea5ff7f3d7439cc812d364cf1bcf15513ba7
Popova Julia <yuliapopova.s@yandex.ru>
2023-11-28 19:41:17 +0300

    Updates

---
+++ static/js/558.eee01d5d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfoodwagon_project=self.webpackChunkfoodwagon_project||[]).push([[558],{5558:function(s,j,e){e.r(j);var n=e(5188),x=e(184);j.default=function(){return(0,x.jsxs)(x.Fragment,{children:[(0,x.jsx)(n.bH,{}),(0,x.jsx)(n.uT,{}),(0,x.jsx)(n.KN,{}),(0,x.jsx)(n.Jj,{}),(0,x.jsx)(n.C8,{}),(0,x.jsx)(n.eB,{}),(0,x.jsx)(n.SJ,{}),(0,x.jsx)(n.Uf,{}),(0,x.jsx)(n.GR,{}),(0,x.jsx)(n.SM,{})]})}}}]);
+//# sourceMappingURL=558.eee01d5d.chunk.js.map
