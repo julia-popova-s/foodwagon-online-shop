0fa497d6ccf5f5c2f91a9daf5cb514b85564e726
Popova Julia <yuliapopova.s@yandex.ru>
2024-01-16 17:14:09 +0300

    added processing of pressing the delete key when entering into the search field

--- src/components/ui/TextInput/TextInput.tsx
+++ src/components/ui/TextInput/TextInput.tsx
@@ -20,6 +20,12 @@ export const TextInput = forwardRef<HTMLDivElement, PropsWithChildren<TextInputP
 
     const [value, setValue] = useState<string>('');
 
+    const handleKeyDeleteDown = (event: KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Delete') {
+        handleClickClear();
+      }
+    };
+    
     const handleClickClear = () => {
       setValue('');
       handleSearchValue('');
@@ -29,7 +35,7 @@ export const TextInput = forwardRef<HTMLDivElement, PropsWithChildren<TextInputP
     const updateSearchValue = useCallback(
       debounce((str: string) => {
         handleSearchValue(str);
-      }, 300),
+      }, 500),
       [],
     );
 
@@ -41,11 +47,14 @@ export const TextInput = forwardRef<HTMLDivElement, PropsWithChildren<TextInputP
     return (
       <div className={cn(style.search, classNames)} ref={ref}>
         <input
+          onKeyDown={(e) => {
+            if (!!handleKeyDown) handleKeyDown(e);
+            handleKeyDeleteDown(e);
+          }}
           autoComplete="off"
           className={style.search__input}
           name="find"
           onChange={handleChangeValue}
-          onKeyDown={handleKeyDown}
           placeholder="Enter your request"
           ref={inputRef}
           type="text"
@@ -53,17 +62,14 @@ export const TextInput = forwardRef<HTMLDivElement, PropsWithChildren<TextInputP
         />
 
         {value && (
-          <svg
-            className={style.search__clearIcon}
-            onClick={handleClickClear}
-            viewBox="0 0 32 32"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g fillRule="evenodd" id="Fail">
-              <path d="m16 3a13 13 0 1 1 -13 13 13.006 13.006 0 0 1 13-13zm0-2a15 15 0 1 0 15 15 15.007 15.007 0 0 0 -15-15z"></path>
-              <path d="m14.586 16-4.293 4.293a1 1 0 0 0 1.414 1.414l4.293-4.293 4.293 4.293a1 1 0 1 0 1.414-1.414l-4.293-4.293 4.293-4.293a1 1 0 0 0 -1.414-1.414l-4.293 4.293-4.293-4.293a1 1 0 0 0 -1.414 1.414z"></path>
-            </g>
-          </svg>
+          <button className={style.search__clearBtn} onClick={handleClickClear}>
+            <svg className={style.search__clearIcon} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <g fillRule="evenodd" id="Fail">
+                <path d="m16 3a13 13 0 1 1 -13 13 13.006 13.006 0 0 1 13-13zm0-2a15 15 0 1 0 15 15 15.007 15.007 0 0 0 -15-15z"></path>
+                <path d="m14.586 16-4.293 4.293a1 1 0 0 0 1.414 1.414l4.293-4.293 4.293 4.293a1 1 0 1 0 1.414-1.414l-4.293-4.293 4.293-4.293a1 1 0 0 0 -1.414-1.414l-4.293 4.293-4.293-4.293a1 1 0 0 0 -1.414 1.414z"></path>
+              </g>
+            </svg>
+          </button>
         )}
 
         {children}
